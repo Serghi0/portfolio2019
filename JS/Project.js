6c6b61c656fc2663bc48dfcef823759a02d834c0
Serghi0 <s_l_l_@msn.com>
2021-01-12 15:58:16 +0100

    temporarily disabled animations on project pages

--- JS/Project.js
+++ JS/Project.js
@@ -1,30 +1,30 @@
-gsap.fromTo("#work-one", {
-    scrollTrigger: "#work-one", // start the animation when ".box" enters the viewport (once)
-    x:-1000,
-    autoAlpha:0
-  },{
-    scrollTrigger: "#work-one",
-    duration:1,
-    x:0,
-    autoAlpha:1
-});
-gsap.fromTo("#work-two", {
-    scrollTrigger: "#work-two", // start the animation when ".box" enters the viewport (once)
-    x:1000,
-    autoAlpha:0
-  },{
-    scrollTrigger: "#work-two",
-    duration:1,
-    x:0,
-    autoAlpha:1
-});
-gsap.fromTo("#work-three", {
-    scrollTrigger: "#work-three", // start the animation when ".box" enters the viewport (once)
-    x:-1000,
-    autoAlpha:0
-  },{
-    scrollTrigger: "#work-three",
-    duration:1,
-    x:0,
-    autoAlpha:1
-});
+// gsap.fromTo("#work-one", {
+//     scrollTrigger: "#work-one", // start the animation when ".box" enters the viewport (once)
+//     x:-1000,
+//     autoAlpha:0
+//   },{
+//     scrollTrigger: "#work-one",
+//     duration:1,
+//     x:0,
+//     autoAlpha:1
+// });
+// gsap.fromTo("#work-two", {
+//     scrollTrigger: "#work-two", // start the animation when ".box" enters the viewport (once)
+//     x:1000,
+//     autoAlpha:0
+//   },{
+//     scrollTrigger: "#work-two",
+//     duration:1,
+//     x:0,
+//     autoAlpha:1
+// });
+// gsap.fromTo("#work-three", {
+//     scrollTrigger: "#work-three", // start the animation when ".box" enters the viewport (once)
+//     x:-1000,
+//     autoAlpha:0
+//   },{
+//     scrollTrigger: "#work-three",
+//     duration:1,
+//     x:0,
+//     autoAlpha:1
+// });
